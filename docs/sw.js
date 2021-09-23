02a5b349ca159e055ad5980fd7d07f4fb8cec8b6
Shard of Honor <47281367+shardofhonor@users.noreply.github.com>
2021-09-23 23:05:46 +0200

    New Core Cache Version

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Names of the caches used in this version of the service worker.
 // Increase chache number when you update any of the local resources, which will in turn trigger the install event again.
-const PRECACHE_CORE = 'precache-core-v15';
+const PRECACHE_CORE = 'precache-core-v16';
 const PRECACHE_CARD = 'precache-card-v2';
 const RUNTIME = 'runtime';
 
